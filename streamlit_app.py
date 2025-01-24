4486fc8817e4364b3ae7df0a6c24e550acb27ca9
Emanuel Abiyo <92691123+emabistar@users.noreply.github.com>
2025-01-24 12:53:54 +0000

    Initial  commit deepseek  and strealit chat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,46 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import tempfile
+api_secret_key = st.secrets["secret_key"]  
+from openai import OpenAI  # Ensure the OpenAI library is installed
+st.title("Emanuel comagnion Friend Chat")
+# Initialise openIA cleant
+client = OpenAI(
+    api_key=api_secret_key,  # Replace with your API key
+    base_url="https://api.deepseek.com"
+    )
+
+
+
+# Initialize session state to store messages
+if "messages"  not in  st.session_state:
+    st.session_state.messages = [
+        {"role":"assistant","content":"You are helpfull assistant  you act as a concierge"}
+    ]
+
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Get user imput
+user_input = st.chat_input("Ask a Question")
+# Ge the user imput( messsage) and  add to the user history
+if user_input:
+    st.session_state.messages.append({"role":"user","content":user_input})
+    with st.chat_message("user"):
+        st.markdown(user_input)
+# Generate the  response from openai
+with st.chat_message("assistant"):
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages = st.session_state.messages,
+        stream = False
+    )
+
+ # Diaplay the chat to the  UI
+    assistant_message = response.choices[0].message.content
+    st.markdown(assistant_message)
+    # Add assistant message to chat history
+    st.session_state.messages.append({"role":"assistant","content":assistant_message})
+    
